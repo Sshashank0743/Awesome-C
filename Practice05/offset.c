412a6157d49546a38f11b7a96d3bb798884ff6e8
shashank shukla <53620609+Sshashank0743@users.noreply.github.com>
2024-10-03 22:04:40 +0530

    Update

---
+++ Practice05/offset.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 8;
+    float b = 2.333;
+    printf("Hello world\n");
+
+    printf("%7.4f", b);
+    return 0;
+}
---
+++ number_table.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a;
+    int b;
+
+    printf("a*b = %d, b++", a,b);
+    return 0;
+}
