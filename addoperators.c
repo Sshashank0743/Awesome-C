319268fc4053d5b0eaae719baffc0dae25407c78
shashank shukla <53620609+Sshashank0743@users.noreply.github.com>
2024-10-03 18:59:46 +0530

    Update

---
+++ addoperators.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    /* code */
+    int a= 3;
+    int b = 5;
+
+    printf("a + b = %d\n", a+b); 
+    printf("a - b = %d\n", a-b); 
+    printf("a * b = %d\n", a*b); 
+    printf("a / b = %d\n", a/b); 
+
+    return 0;
+}
