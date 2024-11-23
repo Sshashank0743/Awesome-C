5d3f3639440b40d36a4c086d0ff44da6f3869150
shashank shukla <53620609+Sshashank0743@users.noreply.github.com>
2024-11-24 00:04:26 +0530

    Loops update

--- LOOPs/pension.c
+++ LOOPs/pension.c
@@ -1,11 +1,12 @@
 int main(int argc, char const *argv[])
 {
-    int num, index=0;
+    int num, index = 0;
     printf("Enter your number\n");
     scanf("%d", &num);
-    do{
-        printf("%d", index+1);
-        index = index +1;
-    }while(index < num);
+    do
+    {
+        printf("%d", index + 1);
+        index = index + 1;
+    } while (index < num);
     return 0;
 }
